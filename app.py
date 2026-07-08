41274e0cfe68e989c59b5b104c8344baaaa3013a
unknown <judsonquaria14@gmail.com>
2026-07-08 15:01:44 +0530

    Update retrained model

--- app.py
+++ app.py
@@ -1,5 +1,33 @@
 import streamlit as st
-import hashlib
+import joblib
+import numpy as np 
 
-with open("gb_booking_model.pkl", "rb") as f:
-    st.write(hashlib.sha256(f.read()).hexdigest())
+model = joblib.load('gb_booking_model.pkl')
+
+st.title("Hotel Booking Cancellation Predictor")
+st.divider()
+st.write("Enter booking details to predict cancellation. ")
+
+lead_time = st.slider("lead Time", 0, 500, 50)
+avg_price = st.number_input("Average Price per Room", 0.0, 500.0, 100.0)
+special_requests = st.slider("Number of Special Requestes", 0, 5, 1)
+total_guests = st.slider("Total Guests", 1, 10, 2)
+total_nights = st.slider("Total Nights", 1, 20, 3)
+repeated_guest = st.selectbox("Repeated Guest", [0,1])
+
+if st.button("Predict"):
+    input_data = np.array([[
+    lead_time,
+    avg_price,
+    special_requests,
+    total_guests,
+    total_nights,
+    repeated_guest
+]])
+    prediction = model.predict(input_data)[0]
+    prob = model.predict_proba(input_data)[0][1]
+
+    if prediction == 1:
+        st.success(f"Prediction: Not canceled (Probability: {prob:.2f})")
+    else:
+        st.error(f"Prediction: Canceled (Probability: {1 - prob:.2f})")
